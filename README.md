a7e04ae14eed06a445de105a10c511a1cbba3a72
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-06-18 00:23:03 +0300

    Add travis config file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-demo
+
+[![Build Status](https://travis-ci.com/shilgam/js-demo.svg?branch=master)](https://travis-ci.com/shilgam/js-demo)
--- capybara/spec/features/first_spec.rb
+++ capybara/spec/features/first_spec.rb
@@ -1,5 +1,5 @@
-feature "Capybara features" do
-  scenario "Fake rspec test" do
+describe "Capybara features" do
+  it "Fake rspec test" do
     expect(1).to eq 1
   end
 end
