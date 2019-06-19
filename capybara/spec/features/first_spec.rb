231cdf0bc50cff20d55b2f81db325270035e65bb
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-06-20 00:07:33 +0300

    Load secret env vars from separate file

--- capybara/spec/features/first_spec.rb
+++ capybara/spec/features/first_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
-describe "Capybara features" do
-  it "Fake rspec test" do
+describe 'Capybara features' do
+  it 'Sign in' do
     visit '/'
     expect(page).to have_css('.form-signin')
+
+    within '.form-signin' do
+      fill_in 'user_email', with: ENV['APP_USERNAME']
+      fill_in 'Password', with: ENV['APP_PASSWORD']
+      click_button 'LOG IN'
+    end
+
+    expect(page).to have_content('Signed in successfully.')
   end
 end
