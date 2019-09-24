b15a66b423cd64d05b1b6787465cd50cbd7fecd0
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-09-24 15:03:54 +0300

    Configure expect-puppeteer

--- puppeteer/lib/jest/configureJest.js
+++ puppeteer/lib/jest/configureJest.js
@@ -1,6 +1,9 @@
 /* global jest */
 
 import config from 'config';
+import setupExpectPuppeteer from '../helper/expectPuppeteer/setup';
 
 const jestConfig = config.get('jest');
 jest.setTimeout(jestConfig.JEST_TIMEOUT);
+
+setupExpectPuppeteer();
